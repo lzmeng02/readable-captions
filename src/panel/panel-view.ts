9983f6bb96de4de42f8d4bcaf657d346bcd69211
lzmeng02 <2750216311@qq.com>
2026-03-04 19:55:54 +0800

    [Refactor] delete

--- src/panel/panel-view.ts
+++
@@ -1,3 +0,0 @@
-export { panelTemplate, type PanelHandlers, type PanelViewModel } from "../ui/components/rc-panel";
-export { panelStyles } from "../ui/styles/panel.css";
-export type { TabId as Mode } from "../shared/types";
